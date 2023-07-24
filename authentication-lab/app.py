0864ab5c2f8a5aa5be67561c44256cbe1e521f3a
EmilyPo24-meet <figidave659@gmail.com>
2023-07-24 12:10:51 +0300

    done

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -21,7 +21,17 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    try:
+        login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+        return redirect(url_for('add_tweet'))
+    except:
+        error = "Authentication failed"
+        return render_template("signin.html")
+
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -32,7 +42,7 @@ def signup():
         password = request.form['password']
     try:
         login_session['user'] = auth.create_user_with_email_and_password(email, password)
-        return redirect(url_for('home'))
+        return redirect(url_for('add_tweet'))
     except:
         error = "Authentication failed"
         return render_template("signup.html")
